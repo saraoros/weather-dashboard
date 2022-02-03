55a3c0a45b952169b0b641e4d266e4d4ab7c66b1
Sara Gochez <saragochez13@gmail.com>
2022-02-03 13:33:07 -0800

    history works, just need to get it to display on page when refreshed

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,52 @@ var searchEl = document.getElementById('searchbtn');
 var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?';
 var apiId = '113200bab49467606bb2319ca3ecb8e8';
 var saveSearch = JSON.parse(localStorage.getItem('search')) || [ ];
-
+var response = cityInputEl;
 // var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + city + '&appid=' + apiId;
 
 
+var getCityName = function (response) {
+    var response = fetch('https://api.openweathermap.org/data/2.5/weather?' + cityInputEl + '&appid=' + apiId).get(function (response) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    });
+  };
+  
+  var getCityWeather = function () {
+    //
+    var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + cityInputEl + '&appid=' + apiId;
+  
+    // make a get request to weather api
+    fetch(weatherApi)
+      .then(function (response) {
+        //
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            var cityName = data.city.name;
+            console.log(data.items);
+            //
+          });
+        } else {
+          alert('Error: ' + response);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to Weather Data');
+      });
+  };
+  
 
-
-
-
-
-
-
-
-
-
-// start of search history, search history doesn't work
+// search history doesn't work
 function citySearchHistory() {
   cityHistoryEl.innerHTML = ' ';
   for (let i = 0; i < saveSearch.length; i++) {
     var historyInput = document.createElement('input');
-    this.setAttribute('type', 'button');
-    this.setAttribute('class', 'form-control d-block bg-white');
-    this.setAttribute('value', saveSearch[i]);
-    this.addEventListener('click', function () {
+    historyInput.setAttribute('type', 'button');
+    historyInput.setAttribute('class', 'form-control d-block bg-white');
+    historyInput.setAttribute('value', saveSearch[i]);
+    historyInput.addEventListener('click', function () {
     getCityWeather(historyInput.value);
     });
     cityHistoryEl.append(historyInput);
@@ -41,6 +64,7 @@ searchEl.addEventListener('click', function () {
   var searchCity = cityInputEl.value;
   getCityWeather(searchCity);
   saveSearch.push(searchCity);
+  localStorage.getItem('search', JSON.stringify(saveSearch));
   localStorage.setItem('search', JSON.stringify(saveSearch));
   citySearchHistory();
 });
