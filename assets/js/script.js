a8cbefe45752455f284088ef52afc4305d57558a
Sara Gochez <saragochez13@gmail.com>
2022-02-03 13:14:42 -0800

    modified history section and restarting on functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,36 @@
+// global variables
 var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#citySearch');
+var currentWeatherEl = document.querySelector('#currentWeather');
 var cityHistoryEl = document.getElementById('history');
 var searchEl = document.getElementById('searchbtn');
-var weatherApi =
-  'api.openweathermap.org/data/2.5/weather?q={city name}&appid={113200bab49467606bb2319ca3ecb8e8}';
-var saveSearch = JSON.parse(localStorage.getItem('search')) || [];
+var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?';
+var apiId = '113200bab49467606bb2319ca3ecb8e8';
+var saveSearch = JSON.parse(localStorage.getItem('search')) || [ ];
 
-var getCityName = function () {
-  var response = fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={113200bab49467606bb2319ca3ecb8e8}'
-  ).get(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  });
-};
+// var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + city + '&appid=' + apiId;
 
-var getWeather = function () {
-  //
-  var weatherApi =
-    'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={113200bab49467606bb2319ca3ecb8e8}';
 
-  // make a get request to weather api
-  fetch(weatherApi)
-    .then(function (response) {
-      //
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          var cityName = data.city.name;
-          console.log(data.items);
-          //
-        });
-      } else {
-        alert('Error: ' + response);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to Weather Data');
-    });
-};
 
-// search history doesn't work
+
+
+
+
+
+
+
+
+
+// start of search history, search history doesn't work
 function citySearchHistory() {
   cityHistoryEl.innerHTML = ' ';
   for (let i = 0; i < saveSearch.length; i++) {
     var historyInput = document.createElement('input');
-    historyInput.setAttribute('type', 'text');
-    historyInput.setAttribute('class', 'form-control d-block bg-white');
-    historyInput.setAttribute('value', saveSearch[i]);
-    historyInput.addEventListener('click', function () {
-      getWeather(historyInput.value);
+    this.setAttribute('type', 'button');
+    this.setAttribute('class', 'form-control d-block bg-white');
+    this.setAttribute('value', saveSearch[i]);
+    this.addEventListener('click', function () {
+    getCityWeather(historyInput.value);
     });
     cityHistoryEl.append(historyInput);
   }
@@ -59,7 +39,7 @@ function citySearchHistory() {
 // save history to localStorage
 searchEl.addEventListener('click', function () {
   var searchCity = cityInputEl.value;
-  getWeather(searchCity);
+  getCityWeather(searchCity);
   saveSearch.push(searchCity);
   localStorage.setItem('search', JSON.stringify(saveSearch));
   citySearchHistory();
