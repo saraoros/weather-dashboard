ffcb0dbf7f4ad6cd3cd421fc8488c450cb287b43
Sara Gochez <saragochez13@gmail.com>
2022-02-03 23:21:41 -0800

    added new LS for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,31 @@ var cityHistoryEl = document.getElementById('history');
 var searchEl = document.getElementById('searchbtn');
 var apiId = '113200bab49467606bb2319ca3ecb8e8';
 var saveSearch = JSON.parse(localStorage.getItem('search')) || [ ];
-var cityNameEl = document.getElementById('cityName');
-var iconEl = document.getElementById('icon');
-var tempEl = document.getElementById('temperature');
-var windEl = document.getElementById('wind');
-var humidityEl = document.getElementById('humidity');
-var uvIndexEl = document.getElementById('uv');
+// var searchButton = $(".searchButton");
 // var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + '&q=' + city +'&units=imperial' + '&appid=' + apiId;
-// var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?';
 
 
+for (var i = 0; i < localStorage.length; i++) {
+  var city = localStorage.getItem(i);
+  var cityName = $(".list-group").addClass("list-group-item");
+  cityName.append("<li>" + city + "</li>");
+}
 
 
+  
 
 
-// search history doesn't work
+// search history doesn't work properly
 function citySearchHistory() {
-  cityHistoryEl.innerHTML = ' ';
+  cityHistoryEl.innerHTML = '';
+
   for (let i = 0; i < saveSearch.length; i++) {
     var historyInput = document.createElement('input');
     historyInput.setAttribute('type', 'button');
     historyInput.setAttribute('class', 'form-control d-block bg-white');
     historyInput.setAttribute('value', saveSearch[i]);
     historyInput.addEventListener('click', function () {
-    getCityWeather(historyInput.value);
+    // getCityWeather(historyInput.value);
     });
     cityHistoryEl.append(historyInput);
   }
@@ -39,15 +40,12 @@ function citySearchHistory() {
 // save history to localStorage
 searchEl.addEventListener('click', function () {
   var searchCity = cityInputEl.value;
-  getCityWeather(searchCity);
+  // getCityWeather(searchCity);
   saveSearch.push(searchCity);
   localStorage.getItem('search', JSON.stringify(saveSearch));
   localStorage.setItem('search', JSON.stringify(saveSearch));
   citySearchHistory();
 })
-
-
-
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -72,4 +70,3 @@ clearButton.addEventListener('click', function () {
 
 userFormEl.addEventListener('submit', formSubmitHandler);
 //getCityWeather();
- 
