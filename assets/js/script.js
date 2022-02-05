22c56a110d4648dac62922daadd8468211bd742e
Sara Gochez <saragochez13@gmail.com>
2022-02-05 14:30:38 -0800

    five day forecast not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,100 @@
 // start of global variable declarations
 var userFormEl = document.querySelector('#user-form');
-var clearButton = document.getElementById('clearBtn')
+var clearButton = document.getElementById('clearBtn');
 var cityInputEl = document.querySelector('#citySearch');
 var currentWeatherEl = document.querySelector('#currentWeather');
 var cityHistoryEl = document.getElementById('history');
 var searchEl = document.getElementById('searchbtn');
 var apiId = '113200bab49467606bb2319ca3ecb8e8';
-var saveSearch = JSON.parse(localStorage.getItem('search')) || [ ];
+var saveSearch = JSON.parse(localStorage.getItem('search')) || [];
 var cityNameEl = document.getElementById('cityName');
 var iconEl = document.getElementById('icon');
 var tempEl = document.getElementById('temperature');
 var windEl = document.getElementById('wind');
 var humidityEl = document.getElementById('humidity');
-var uvIndexEl = document.getElementById('uv');
-
-// var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + '&q=' + city +'&units=imperial' + '&appid=' + apiId;
+var fiveDayAheadEl = document.getElementById('fiveday');
 
 var getCityWeather = function (cityName) {
   // format the api url
-  var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + '&q=' + cityName +'&units=imperial' + '&appid=' + apiId;
-axios.get(weatherApi).then(function (response) {
-  // make a get request to weather api
- 
-   currentWeatherEl.classList.remove("d-none");
-   
-  
-  // start of B.C.
-var currentDate = new Date(response.data.dt * 1000);
-var day = currentDate.getDate();
-var month = currentDate.getMonth() + 1;
-var year = currentDate.getFullYear();
-cityNameEl.innerHTML = response.data.name + " " + month + "/" + day + "/" + year;
+  var weatherApi =
+    'https://api.openweathermap.org/data/2.5/weather?' +
+    '&q=' +
+    cityName +
+    '&units=imperial' +
+    '&appid=' +
+    apiId;
+  axios.get(weatherApi).then(function (response) {
+    currentWeatherEl.classList.remove('d-none');
 
+    // start of B.C.
+    var currentDate = new Date(response.data.dt * 1000);
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    cityNameEl.innerHTML =
+      response.data.name + ' ' + month + '/' + day + '/' + year;
 
-// weather icons
-var currentWeatherIcon = response.data.weather[0].icon;
-iconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png");
-iconEl.setAttribute("alt", response.data.weather[0].description);
-tempEl.innerHTML = "Temperature: " + (response.data.main.temp) + " &#176F";
-humidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-windEl.innerHTML = "Wind: " + response.data.wind.speed + " MPH";
-        
+    // start of weather icons
+    var currentWeatherIcon = response.data.weather[0].icon;
+    iconEl.setAttribute(
+      'src',
+      'https://openweathermap.org/img/wn/' + currentWeatherIcon + '@2x.png'
+    );
+    iconEl.setAttribute('alt', response.data.weather[0].description);
+    tempEl.innerHTML = 'Temperature: ' + response.data.main.temp + ' &#176F';
+    humidityEl.innerHTML = 'Humidity: ' + response.data.main.humidity + '%';
+    windEl.innerHTML = 'Wind: ' + response.data.wind.speed + ' MPH';
 
- // lat & lon
+    // lat & lon
+    var lat = response.data.coord.lat;
+    var lon = response.data.coord.lon;
+    var latLonUrl =
+      'https://api.openweathermap.org/data/2.5/uvi/forecast?lat=' +
+      lat +
+      '&lon=' +
+      lon +
+      '&appid=' +
+      apiId;
+    axios.get(latLonUrl);
+  });
 
- var lat = response.data.coord.lat;
- var lon = response.data.coord.lon;
- var latLonUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiId;
- axios.get(latLonUrl)
+  // Five day ahead api
+  var cityFiveDay = response.data.id;
+  var fiveDayUrl =
+    'https://api.openweathermap.org/data/2.5/forecast?id=' +
+    cityFiveDay +
+    '&units=imperial' +
+    '&appid=' +
+    apiId;
+  axios
+    .get(fiveDayUrl)
 
-
-});
-    }
-
+    .then(function (response) {
+      fiveDayAheadEl.classList.remove('d-none');
+      var fiveForeDivs = document.querySelectorAll('.fiveForecast');
+      for (i = 0; i < fiveForeDivs.length; i++) {
+        fiveForeDivs[i].innerHTML = " ";
+        var fiveIndex = i * 8 + 4;
+        var fiveDates = new Date(response.data.list[fiveIndex].dt * 1000);
+        var fiveForecastDay = fiveDates.getDate();
+        var fiveForecastMonth = fiveDates.getMonth() + 1;
+        var fiveForecastYear = fiveDates.getFullYear();
+        var showFiveDays = document.createElement('p');
+        showFiveDays.setAttribute('class', 'mt-3  fiveDates');
+        showFiveDays.innerHTML =
+          fiveForecastMonth + ' /' + fiveForecastDay + '/' + fiveForecastYear;
+        fiveForeDivs[i].append(showFiveDays);
+      }
+    });
+};
 
 var displayWeatherError = function (currentTemp) {
-if (currentTemp.length === 0) {
-  currentWeatherEl.textContent = 'We are unable to connect to the weather data at this time.';
-  return;
-}
-}
-
-
-  
+  if (currentTemp.length === 0) {
+    currentWeatherEl.textContent =
+      'We are unable to connect to the weather data at this time.';
+    return;
+  }
+};
 
 
 // start of search history
@@ -75,7 +107,7 @@ function citySearchHistory() {
     historyInput.setAttribute('class', 'form-control d-block bg-white');
     historyInput.setAttribute('value', saveSearch[i]);
     historyInput.addEventListener('click', function () {
-    getCityWeather(historyInput.value);
+      getCityWeather(historyInput.value);
     });
     cityHistoryEl.append(historyInput);
   }
@@ -89,7 +121,7 @@ searchEl.addEventListener('click', function () {
   localStorage.getItem('search', JSON.stringify(saveSearch));
   localStorage.setItem('search', JSON.stringify(saveSearch));
   citySearchHistory();
-})
+});
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -102,15 +134,12 @@ var formSubmitHandler = function (event) {
   console.log(event);
 };
 
-
 // Clear History button
-clearButton.addEventListener('click', function() {
+clearButton.addEventListener('click', function () {
   localStorage.clear();
   cityHistoryEl.remove();
   searchHistory = [];
   citySearchHistory();
-})
-
-
+});
 
 userFormEl.addEventListener('submit', formSubmitHandler);
