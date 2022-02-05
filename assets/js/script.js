b66c8d2510b9e824c18c5db566598f0fdab29bf3
Sara Gochez <saragochez13@gmail.com>
2022-02-05 00:25:49 -0800

    finally got the clear history button to work and got the card to show current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,57 @@ var cityHistoryEl = document.getElementById('history');
 var searchEl = document.getElementById('searchbtn');
 var apiId = '113200bab49467606bb2319ca3ecb8e8';
 var saveSearch = JSON.parse(localStorage.getItem('search')) || [ ];
-// var searchButton = $(".searchButton");
+var cityNameEl = document.getElementById('cityName');
+var iconEl = document.getElementById('icon');
+var tempEl = document.getElementById('temperature');
+var windEl = document.getElementById('wind');
+var humidityEl = document.getElementById('humidity');
+var uvIndexEl = document.getElementById('uv');
+
 // var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + '&q=' + city +'&units=imperial' + '&appid=' + apiId;
 
+var getCityWeather = function (cityName) {
+  // format the api url
+  var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?' + '&q=' + cityName +'&units=imperial' + '&appid=' + apiId;
+axios.get(weatherApi).then(function (response) {
+  // make a get request to weather api
+ 
+   currentWeatherEl.classList.remove("d-none");
 
-for (var i = 0; i < localStorage.length; i++) {
-  var city = localStorage.getItem(i);
-  var cityName = $(".list-group").addClass("list-group-item");
-  cityName.append("<li>" + city + "</li>");
+
+// weather icons
+var currentWeatherIcon = response.data.weather[0].icon;
+iconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png");
+iconEl.setAttribute("alt", response.data.weather[0].description);
+tempEl.innerHTML = "Temperature: " + (response.data.main.temp) + " &#176F";
+humidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+windEl.innerHTML = "Wind: " + response.data.wind.speed + " MPH";
+        
+
+ // lat & lon
+
+ var lat = response.data.coord.lat;
+ var lon = response.data.coord.lon;
+ var latLonUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiId;
+ axios.get(latLonUrl)
+
+
+});
+    }
+
+
+var displayWeatherError = function (currentTemp) {
+if (currentTemp.length === 0) {
+  currentWeatherEl.textContent = 'We are unable to connect to the weather data at this time.';
+  return;
+}
 }
 
 
   
 
 
-// search history doesn't work properly
+// start of search history
 function citySearchHistory() {
   cityHistoryEl.innerHTML = '';
 
@@ -31,7 +67,7 @@ function citySearchHistory() {
     historyInput.setAttribute('class', 'form-control d-block bg-white');
     historyInput.setAttribute('value', saveSearch[i]);
     historyInput.addEventListener('click', function () {
-    // getCityWeather(historyInput.value);
+    getCityWeather(historyInput.value);
     });
     cityHistoryEl.append(historyInput);
   }
@@ -40,7 +76,7 @@ function citySearchHistory() {
 // save history to localStorage
 searchEl.addEventListener('click', function () {
   var searchCity = cityInputEl.value;
-  // getCityWeather(searchCity);
+  getCityWeather(searchCity);
   saveSearch.push(searchCity);
   localStorage.getItem('search', JSON.stringify(saveSearch));
   localStorage.setItem('search', JSON.stringify(saveSearch));
@@ -60,8 +96,9 @@ var formSubmitHandler = function (event) {
 
 
 // Clear History button
-clearButton.addEventListener('click', function () {
+clearButton.addEventListener('click', function() {
   localStorage.clear();
+  cityHistoryEl.remove();
   searchHistory = [];
   citySearchHistory();
 })
@@ -69,4 +106,3 @@ clearButton.addEventListener('click', function () {
 
 
 userFormEl.addEventListener('submit', formSubmitHandler);
-//getCityWeather();
