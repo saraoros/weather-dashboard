3e9702485d2ddd4df6605c76b8e1712093dc6f9e
Sara Gochez <saragochez13@gmail.com>
2022-02-06 15:56:19 -0800

    got five days to show and finished README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# weather-dashboard
+# Weather Dashboard
+ The goal of this challenge was to allow travelers to see the weather outlook for multiple cities so that they can plan a trip accordingly.
+
+ ## Challenge Acceptance Criteria
+
+- GIVEN a weather dashboard with form inputs
+- WHEN I search for a city
+- THEN I am presented with current and future conditions for that city and that city is added to the search history
+- WHEN I view current weather conditions for that city
+- THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+- WHEN I view the UV index
+- THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+- WHEN I view future weather conditions for that city
+- THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+- WHEN I click on a city in the search history
+- THEN I am again presented with current and future conditions for that city`
+
+
+### Deployed Application: https://saraoros.github.io/weather-dashboard/
+
+<img src="./assets/images/headerss.PNG" />
+
+<img src ="./assets/images/fivedayss.PNG" />
+
+
+### Personal Challenges While Working On App:
+
+This project truly challenged me in so many ways. Although it was greatly challenging and very time consuming, I also feel like I have learned so much! This challenge forced me to really go above and beyond on reading API documentations properly, learning new ways to code like using 'axios', and watched numerous videos on examples on how to properly use and get information from API's. Although there were many victories on here, there are still some things that need to be improved. I look forward to being able to improve this project in the future.
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,14 @@ var tempEl = document.getElementById('temperature');
 var windEl = document.getElementById('wind');
 var humidityEl = document.getElementById('humidity');
 var fiveDayAheadEl = document.getElementById('fiveday');
+var uvEl = document.getElementById('uv');
 
+
+
+// get cities' weather
 var getCityWeather = function (cityName) {
-  // format the api url
   var weatherApi =
-    'https://api.openweathermap.org/data/2.5/weather?' +
-    '&q=' +
-    cityName +
-    '&units=imperial' +
-    '&appid=' +
-    apiId;
+    'https://api.openweathermap.org/data/2.5/weather?' + '&q=' + cityName + '&units=imperial' + '&appid=' + apiId;
   axios.get(weatherApi).then(function (response) {
     currentWeatherEl.classList.remove('d-none');
 
@@ -31,10 +29,9 @@ var getCityWeather = function (cityName) {
     var day = currentDate.getDate();
     var month = currentDate.getMonth() + 1;
     var year = currentDate.getFullYear();
-    cityNameEl.innerHTML =
-      response.data.name + ' ' + month + '/' + day + '/' + year;
+    cityNameEl.innerHTML = response.data.name + ' ' + month + '/' + day + '/' + year;
 
-    // start of weather icons
+    //weather icons
     var currentWeatherIcon = response.data.weather[0].icon;
     iconEl.setAttribute(
       'src',
@@ -45,7 +42,7 @@ var getCityWeather = function (cityName) {
     humidityEl.innerHTML = 'Humidity: ' + response.data.main.humidity + '%';
     windEl.innerHTML = 'Wind: ' + response.data.wind.speed + ' MPH';
 
-    // lat & lon
+    // get lat & lon
     var lat = response.data.coord.lat;
     var lon = response.data.coord.lon;
     var latLonUrl =
@@ -55,47 +52,63 @@ var getCityWeather = function (cityName) {
       lon +
       '&appid=' +
       apiId;
-    axios.get(latLonUrl);
-  });
-
-  // Five day ahead api
-  var cityFiveDay = response.data.id;
-  var fiveDayUrl =
-    'https://api.openweathermap.org/data/2.5/forecast?id=' +
-    cityFiveDay +
-    '&units=imperial' +
-    '&appid=' +
-    apiId;
-  axios
-    .get(fiveDayUrl)
-
-    .then(function (response) {
-      fiveDayAheadEl.classList.remove('d-none');
-      var fiveForeDivs = document.querySelectorAll('.fiveForecast');
-      for (i = 0; i < fiveForeDivs.length; i++) {
-        fiveForeDivs[i].innerHTML = " ";
-        var fiveIndex = i * 8 + 4;
-        var fiveDates = new Date(response.data.list[fiveIndex].dt * 1000);
-        var fiveForecastDay = fiveDates.getDate();
-        var fiveForecastMonth = fiveDates.getMonth() + 1;
-        var fiveForecastYear = fiveDates.getFullYear();
-        var showFiveDays = document.createElement('p');
-        showFiveDays.setAttribute('class', 'mt-3  fiveDates');
-        showFiveDays.innerHTML =
-          fiveForecastMonth + ' /' + fiveForecastDay + '/' + fiveForecastYear;
-        fiveForeDivs[i].append(showFiveDays);
-      }
+    axios.get(latLonUrl).then(function (response) {
+      var UVIndex = document.createElement('span');
+      UVIndex.innerHTML = response.data[0].value;
+      uvEl.innerHTML = 'UV Index: ';
+      uvEl.append(UVIndex);
     });
-};
 
-var displayWeatherError = function (currentTemp) {
-  if (currentTemp.length === 0) {
-    currentWeatherEl.textContent =
-      'We are unable to connect to the weather data at this time.';
-    return;
-  }
-};
+    // cities weather 5-day forecast
+    var cityFiveDay = response.data.id;
+    var fiveDayUrl =
+      'https://api.openweathermap.org/data/2.5/forecast?id=' +
+      cityFiveDay +
+      '&units=imperial' +
+      '&appid=' +
+      apiId;
+    axios
+      .get(fiveDayUrl).then(function (response) {
+        fiveDayAheadEl.classList.remove('d-none');
+        var fiveForeDivs = document.querySelectorAll('.fiveForecast');
+        for (i = 0; i < fiveForeDivs.length; i++) {
+          fiveForeDivs[i].innerHTML = ' ';
+          var fiveIndex = i * 8 + 4;
+          var fiveDates = new Date(response.data.list[fiveIndex].dt * 1000);
+          var fiveForecastDay = fiveDates.getDate();
+          var fiveForecastMonth = fiveDates.getMonth() + 1;
+          var fiveForecastYear = fiveDates.getFullYear();
+          var showFiveDays = document.createElement('p');
+          showFiveDays.setAttribute('class', 'mt-3  fiveDates');
+          showFiveDays.innerHTML =
+            fiveForecastMonth + '/' + fiveForecastDay + '/' + fiveForecastYear;
+          fiveForeDivs[i].append(showFiveDays);
 
+          var citiesFiveForecast = document.createElement('img');
+          citiesFiveForecast.setAttribute(
+            'src',
+            'https://openweathermap.org/img/wn/' +
+              response.data.list[fiveIndex].weather[0].icon +
+              '@2x.png'
+          );
+          citiesFiveForecast.setAttribute(
+            'alt',
+            response.data.list[fiveIndex].weather[0].description
+          );
+          fiveForeDivs[i].append(citiesFiveForecast);
+
+          var fiveTempEl = document.createElement('p');
+          fiveTempEl.innerHTML =
+            'Temp: ' + response.data.list[fiveIndex].main.temp + ' &#176F';
+          fiveForeDivs[i].append(fiveTempEl);
+          var fiveHumidityEl = document.createElement('p');
+          fiveHumidityEl.innerHTML =
+            'Humidity: ' + response.data.list[fiveIndex].main.humidity + '%';
+          fiveForeDivs[i].append(fiveHumidityEl);
+        }
+      });
+  });
+};
 
 // start of search history
 function citySearchHistory() {
@@ -107,7 +120,7 @@ function citySearchHistory() {
     historyInput.setAttribute('class', 'form-control d-block bg-white');
     historyInput.setAttribute('value', saveSearch[i]);
     historyInput.addEventListener('click', function () {
-      getCityWeather(historyInput.value);
+    getCityWeather(historyInput.value);
     });
     cityHistoryEl.append(historyInput);
   }
@@ -125,11 +138,12 @@ searchEl.addEventListener('click', function () {
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
-
   var cityName = cityInputEl.value.trim();
 
   if (cityName) {
-    //   get
+  
+  } else {
+      alert('Unable to connect to Weather Data');
   }
   console.log(event);
 };
